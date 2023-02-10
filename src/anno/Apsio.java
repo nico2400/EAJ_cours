98bf4a5406aa046bf9322860f2cedaed44621545
nico2400 <78485840+nico2400@users.noreply.github.com>
2023-02-10 11:55:36 +0100

    up

--- src/anno/Apsio.java
+++ src/anno/Apsio.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD,ElementType.CONSTRUCTOR, ElementType.TYPE,ElementType.FIELD,ElementType.PARAMETER })
 @Documented // mattrea nnotation dans la javadoc
 @Inherited //utilisation implicite sur les classes filles
-@Retention(RetentionPolicy.RUNTIME) //
+@Retention(RetentionPolicy.RUNTIME) // RunTime == présent en mémoire
 public @interface Apsio {
     String value() default "inconnu";
     int annee() default 2007;
